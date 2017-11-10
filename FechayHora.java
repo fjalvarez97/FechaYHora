3ab95b30f174f77bab0c5438d0ff3ecc3a56603f
fjalvarez97 <franjalvarez955@gmail.com>
2017-11-10 12:56:20 +0100

    He añadido las clases ClockDisplay y NumberDisplay de otro proyecto de BlueJ

--- FechayHora.java
+++ FechayHora.java
@@ -2,13 +2,14 @@
 /**
  * Write a description of class FechayHora here.
  *
- * @author (your name)
+ * @Fran Alvarez (your name)
  * @version (a version number or a date)
  */
 public class FechayHora
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private CalendarioBasico fecha;
+    private ClockDisplay hora;
 
     /**
      * Constructor for objects of class FechayHora
@@ -16,18 +17,6 @@ public class FechayHora
     public FechayHora()
     {
         // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        
     }
 }
