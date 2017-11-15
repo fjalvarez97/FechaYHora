6cc0662ea98d54f07faacbdf0ac471a1c6d3315f
fjalvarez97 <franjalvarez955@gmail.com>
2017-11-15 09:44:37 +0100

    He añadido dos metodos que avancen la fecha/hora y que la fijen

--- FechayHora.java
+++ FechayHora.java
@@ -7,25 +7,48 @@
  */
 public class FechayHora
 {
-    // instance variables - replace the example below with your own
+    // instance variables
     private CalendarioBasico fecha;
+    // Fecha en formato 8 caracteres
     private ClockDisplay hora;
-
+    // Hora en formato 00:00
+    private int day;
+    private int month;
+    private int year;
+    private int hour;
+    private int min;
     /**
      * Constructor for objects of class FechayHora
      */
     public FechayHora()
     {
+        // Se crea un objeto con valores 01-01-01 para fecha y 00:00 para hora
         fecha = new CalendarioBasico ();
         hora = new ClockDisplay();
     }
     
     public String getFechayHora()
     {
+        //Devuelve la fecha y la hora
         String devolverf = fecha.obtenerFecha();
         String devolverh = hora.getTime();
         
         return devolverf + " " + devolverh;
         
     }
+    
+    public void avanzar()
+    {
+        fecha.avanzarFecha();
+        hora.timeTick();
+    }
+    
+    public void fijarFechaYHora(int daay, int moonth, int yeaar, int miin, int houur)
+    {
+        fecha.fijarFecha(daay,moonth,yeaar);
+        hora.setTime(miin,houur);
+      
+    }
+    
+    
 }
